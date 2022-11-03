4a899a7ab5b8b0921811c7c722db1e0c57314d12
JEEVAN <jeevan2319@gmail.com>
2022-11-03 07:40:14 +0530

    13th commit

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,10 +1,28 @@
 #include<iostream>
 using namespace std;
 
+
+    int fun(int a,int b){
+            return a+b;
+
+    }
+
+
+    int fun(int a,int b, int c){
+
+            
+            return a+b+c;
+    }
+
+
 int main(){
 
-                
+        int y,z;
+        y = fun(5,6);
+        z  = fun(5,6,7);
 
+        cout<<y<<endl;
+        cout<<z<<endl;
 
 
 
